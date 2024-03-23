d02e09aea1de18921cc232bdcad023b18801f45c
fy17ohhh <hester03sfy@gmail.com>
2024-03-23 16:01:34 +0800

    Initialize backend template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science project to enhance the manpower management efficiency of the GoodOldDays restaurant through manpower scheduling optimization using advanced statistical methods',
+    author='Byte Force',
+    license='',
+)
